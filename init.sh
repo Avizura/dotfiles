4ee00c02ea0eaa80091d860c9949ca376c743df4
Michael <michael.dovzhenko@gmail.com>
2022-12-06 20:10:06 +0200

    update brewfile

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+# Install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+## Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
